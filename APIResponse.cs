315ed6940d8a72b6fe2a4ac8a78ade5c97a30a15
williamt6969 <williamtao56@gmail.com>
2024-12-13 12:10:14 +0100

    Add files via upload

---
+++ APIResponse.cs
@@ -0,0 +1,22 @@
+﻿using PogodnaPogoda;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PogodnaPogoda
+{
+    internal class APIResponse
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public double generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public double elevation { get; set; }
+        public CurrentUnits current_units { get; set; }
+        public Current current { get; set; }
+    }
+}
---
+++ CurrentUnits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PogodnaPogoda
+{
+    internal class CurrentUnits
+    {
+        public string time { get; set; }
+        public string interval { get; set; }
+        public string temperature_2m { get; set; }
+        public string relative_humidity_2m { get; set; }
+        public string weather_code { get; set; }
+        public string surface_pressure { get; set; }
+        public string wind_speed_10m { get; set; }
+        public string wind_direction_10m { get; set; }
+    }
+}
